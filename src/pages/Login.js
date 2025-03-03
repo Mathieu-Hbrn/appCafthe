72b8f1f30b2b986be073eb8498656660ed3add21
mathieu.houbron <mathieu.houbron@campus-centre.fr>
2025-03-03 10:56:52 +0100

    commit page account in progress, register allready in progress

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,78 +1,75 @@
-import React, {useContext, useState} from 'react';
-import {AuthContext} from "../context/AuthContext";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-
+import React, { useContext, useState } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Login(props) {
-    const {login} = useContext(AuthContext); // Fonction login venant du contexte
-    const navigate = useNavigate(); // La navigation
+    const { login } = useContext(AuthContext);
+    const navigate = useNavigate();
 
-    const [email, setEmail] = useState("")
-    const [mot_de_passe, setMot_de_passe] = useState("")
-    const [errorMsg, setErrorMsg] = useState("")
-    const [nom_prenom, setNom_prenom] = useState("")
-    const [telephone, setTelephone] = useState("")
-    const [mail_register, setMail_register] = useState("")
-    const [adresse, setAdresse] = useState("")
-    const [mdp_register, setMdp_register] = useState("")
+    const [email, setEmail] = useState('');
+    const [mot_de_passe, setMot_de_passe] = useState('');
+    const [errorMsgLogin, setErrorMsgLogin] = useState('');
+    const [nom_prenom, setNom_prenom] = useState('');
+    const [telephone, setTelephone] = useState('');
+    const [mail_register, setMail_register] = useState('');
+    const [adresse, setAdresse] = useState('');
+    const [mdp_register, setMdp_register] = useState('');
+    const [errorMsgRegister, setErrorMsgRegister] = useState('');
+
     const dateCreation = new Date();
     const year = dateCreation.getFullYear();
-    const month = String(dateCreation.getMonth() + 1).padStart(2, '0'); // Ajoute un zéro si nécessaire
-    const day = String(dateCreation.getDate()).padStart(2, '0'); // Ajoute un zéro si nécessaire
+    const month = String(dateCreation.getMonth() + 1).padStart(2, '0');
+    const day = String(dateCreation.getDate()).padStart(2, '0');
     const dateFormat = `${year}-${month}-${day}`;
 
 
-
-    const handleSubmit = async (e) =>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        setErrorMsg("")
+        setErrorMsgLogin('');
 
         try {
-            const response = await axios.post("http://localhost:3000/api/client/login",
-                {
-                    email, mot_de_passe,
-                },
-            );
-
-            const {token, client} = response.data;
-
-
-            //On met a jour le contexte d'authentification
-            login(token, client);
-
-
-            //Redirection du cient vers une page
-            navigate("/");
-        } catch (error) {
-            console.error("Erreur lors de la connexion : ", error)
-            if (error.response.data.message) {
-                setErrorMsg(error.response.data.message);
-            } else {
-                setErrorMsg("Erreur")
-            }
-        }
-    };
-    const registerSubmit = async (e) =>{
-        e.preventDefault();
-        setErrorMsg("")
-
-        try {
-            const response = await axios.post("http://localhost:3000/api/client/register",
-                {
-                    nom_prenom, telephone, dateFormat, mail_register, mdp_register, adresse
-                },
-            );
+            const response = await axios.post('http://localhost:3000/api/client/login', {
+                Mail_client: email, // Correction du nom du champ
+                mdp_client: mot_de_passe, // Correction du nom du champ
+            });
 
             const { token, client } = response.data;
             login(token, client);
-            navigate("/");
+            navigate('/');
         } catch (error) {
-            console.error("Erreur lors de l'inscription : ", error);
+            console.error('Erreur lors de la connexion : ', error);
             if (error.response && error.response.data && error.response.data.message) {
-                setErrorMsg(error.response.data.message);
+                setErrorMsgLogin(error.response.data.message);
             } else {
-                setErrorMsg("Erreur lors de l'inscription");
+                setErrorMsgLogin('Erreur lors de la connexion');
+            }
+        }
+    };
+
+    const registerSubmit = async (e) => {
+        e.preventDefault();
+        setErrorMsgRegister('');
+
+        try {
+            const response = await axios.post("http://localhost:3000/api/client/register", {
+                nom_prenom_client: nom_prenom,
+                Telephone_client: telephone,
+                Date_inscription_client: dateFormat,
+                Mail_client: mail_register,
+                mdp_client: mdp_register,
+                adresse_client: adresse,
+            });
+
+            const { token, client } = response.data;
+            login(token, client);
+            navigate('/');
+        } catch (error) {
+            console.error('Erreur lors de l\'inscription : ', error);
+            if (error.response && error.response.data && error.response.data.message) {
+                setErrorMsgRegister(error.response.data.message);
+            } else {
+                setErrorMsgRegister('Erreur lors de l\'inscription');
             }
         }
     };
@@ -80,19 +77,20 @@ function Login(props) {
     return (
         <div className="div-container">
             <div>
-                <h2> Connexion</h2>
+                <h2>Connexion</h2>
                 <form onSubmit={handleSubmit}>
                     <ul>
                         <li>
-                            <label> E-mail</label>
-                            <input type="email"
-                                   value={email}
-                                   onChange={(e) => setEmail(e.target.value)}
-                                   required
-                                   />
+                            <label>E-mail</label>
+                            <input
+                                type="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
                         </li>
                         <li>
-                            <label> Mot de passe</label>
+                            <label>Mot de passe</label>
                             <input
                                 type="password"
                                 value={mot_de_passe}
@@ -102,56 +100,71 @@ function Login(props) {
                         </li>
                         <li>
                             <div className="error-message">
-                                {errorMsg && (
-                                    <div>Erreur</div>
-                                )}
+                                {errorMsgLogin && <div>{errorMsgLogin}</div>}
                             </div>
                             <button>Se connecter</button>
                         </li>
                     </ul>
-
                 </form>
             </div>
             <div>
                 <h2>Créer votre compte</h2>
                 <form onSubmit={registerSubmit}>
-                    <li>
-                        <label> Votre nom et prénom </label>
-                        <input type="text" required
-                               value={nom_prenom}
-                               onChange={(e) =>setNom_prenom(e.target.value)}/>
-                        <label> Votre numéro de téléphone</label>
-                        <input type="number" required
-                               value={telephone}
-                               onChange={(e) =>setTelephone(e.target.value)}/>
-                        <label> Votre E-mail</label>
-                        <input type="email"
-                               value={mail_register}
-                               onChange={(e) => setMail_register(e.target.value)}
-                               required
-                        />
-                        <label> Votre adresse</label>
-                        <input type="text"
-                               value={adresse}
-                               onChange={(e) => setAdresse(e.target.value)}
-                               required
-                        />
-                        <label>Votre mot de passe</label>
-                        <input type={"password"}
-                               value={mdp_register}
-                               onChange={(e) => setMdp_register(e.target.value)}/>
-
-                        <div className="error-message">
-                            {errorMsg && (
-                                <div>Erreur</div>
-                            )}
-                        </div>
-                        <button>Créer votre compte</button>
-                    </li>
+                    <ul>
+                        <li>
+                            <label>Votre nom et prénom</label>
+                            <input
+                                type="text"
+                                required
+                                value={nom_prenom}
+                                onChange={(e) => setNom_prenom(e.target.value)}
+                            />
+                        </li>
+                        <li>
+                            <label>Votre numéro de téléphone</label>
+                            <input
+                                type="text" // Changement de type à text
+                                required
+                                value={telephone}
+                                onChange={(e) => setTelephone(e.target.value)}
+                            />
+                        </li>
+                        <li>
+                            <label>Votre E-mail</label>
+                            <input
+                                type="email"
+                                value={mail_register}
+                                onChange={(e) => setMail_register(e.target.value)}
+                                required
+                            />
+                        </li>
+                        <li>
+                            <label>Votre adresse</label>
+                            <input
+                                type="text"
+                                value={adresse}
+                                onChange={(e) => setAdresse(e.target.value)}
+                                required
+                            />
+                        </li>
+                        <li>
+                            <label>Votre mot de passe</label>
+                            <input
+                                type="password"
+                                value={mdp_register}
+                                onChange={(e) => setMdp_register(e.target.value)}
+                            />
+                        </li>
+                        <li>
+                            <div className="error-message">
+                                {errorMsgRegister && <div>{errorMsgRegister}</div>}
+                            </div>
+                            <button>Créer votre compte</button>
+                        </li>
+                    </ul>
                 </form>
             </div>
         </div>
-
     );
 }
 
